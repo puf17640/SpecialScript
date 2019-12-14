f43cc7e6b9eb8071b58b159950dfc934b9c8ac2d
Julian Pufler <puf17640@spengergasse.at>
2019-12-14 11:48:25 +0100

    forgot to remove debugger statement, whoops

--- specialscript.js
+++ specialscript.js
@@ -1,11 +1,7 @@
 const map = {};
-
 const __ = n => n == 0 ? '+[]' : Array.from({length: n}, () => '+!![]').join(' + ');
-
-const _ = s => s.split('').map(x => !(x in map) ? `([]+[])[${_('constructor')}][${_('fromCharCode')}](${__(x.charCodeAt(0))})`: map[x]).join('+');
-
-const compile = code => `(()=>{})[${_('constructor')}](${_(code)})()`;
-
+const _ = s => s.split('').map(x => !(x in map) ? `([]+[])[${map.constructor}][${map.fromCharCode}](${__(x.charCodeAt(0))})`: map[x]).join('+');
+const compile = code => `(()=>{})[${map.constructor}](${_(code)})()`;
 map.a = `(+{}+[])[${__(1)}]`;
 map.b = `({}+[])[${__(2)}]`;
 map.o = `({}+[])[${__(1)}]`;
@@ -20,13 +16,14 @@ map.r = `(!![]+[])[${__(1)}]`;
 map.u = `(!![]+[])[${__(2)}]`;
 map.i = `((+!![]/+[])+[])[${__(3)}]`;
 map.n = `((+!![]/+[])+[])[${__(4)}]`;
-map.S = `([]+([]+[])[${_('constructor')}])[${__(9)}]`;
-map.g = `([]+([]+[])[${_('constructor')}])[${__(14)}]`;
-map.p = `([]+(/-/)[${_('constructor')}])[${__(14)}]`;
+map.constructor = _('constructor')
+map.S = `([]+([]+[])[${map.constructor}])[${__(9)}]`;
+map.g = `([]+([]+[])[${map.constructor}])[${__(14)}]`;
+map.p = `([]+(/-/)[${map.constructor}])[${__(14)}]`;
 map['\\'] = `(/\\\\/+[])[${__(1)}]`;
 map.d = `(${__(13)})[${_('toString')}](${__(14)})`;
 map.h = `(${__(17)})[${_('toString')}](${__(18)})`;
 map.m = `(${__(22)})[${_('toString')}](${__(23)})`;
-map.C = `((()=>{})[${_('constructor')}](${_('return escape')})()(${map['\\']}))[${__(2)}]`;
-
+map.C = `((()=>{})[${map.constructor}](${_('return escape')})()(${map['\\']}))[${__(2)}]`;
+map.fromCharCode = _('fromCharCode')
 export default compile
